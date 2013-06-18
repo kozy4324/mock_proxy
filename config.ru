05f6f99d13706fd54a3b99da02a5e3e4fea9a5c9
Koji NAKAMURA <kozy4324@gmail.com>
2013-06-18 15:31:06 +0900

    enable settings from config.ru.

--- config.ru
+++ config.ru
@@ -1,2 +1,5 @@
+# This file is used by Rack-based servers to start the application.
+#\ --port 3333
 require './lib/mock_proxy'
+MockProxy::App[:cache_path] = './.cache'
 run MockProxy::App
--- lib/mock_proxy.rb
+++ lib/mock_proxy.rb
@@ -7,13 +7,29 @@ module MockProxy
 
   class App < Sinatra::Base
 
-    file_cache = ActiveSupport::Cache::FileStore.new('./.cache')
-    mem_cache = ActiveSupport::Cache::MemoryStore.new(:site => 32.megabytes)
+    set :opt, {
+      :cache_path => './.cache',
+      :mem_cache_size => 32.megabytes
+    }
+
+    def App.[]=(key, value)
+      opt[key] = value
+    end
+
+    file_cache = nil
+    set :file_cache do
+      file_cache ||= ActiveSupport::Cache::FileStore.new(opt[:cache_path])
+    end
+
+    mem_cache = nil
+    set :mem_cache do
+      mem_cache ||= ActiveSupport::Cache::MemoryStore.new(:size => opt[:mem_cache_size])
+    end
     
     get /.*/ do
       cache_key = "#{request.path}?#{request.query_string}"
-      mem_cache.fetch cache_key do
-        file_cache.fetch cache_key do
+      settings.mem_cache.fetch cache_key do
+        settings.file_cache.fetch cache_key do
           "Heeeeeeello\n"
         end
       end
